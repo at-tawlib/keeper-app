ae9d4f3f49c9a82bee035ae14b90bdc9ed7163a3
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-05-02 09:53:00 +0000

    C

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Title</h1>
+            <p>some conent sohoueld jgl ehere tho shoe ahao wnt hla jekla oputoej</p>
+        </div>
+    );
+}
+
+export default Note;
